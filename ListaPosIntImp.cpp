22e96b4e12410273b10c335af9c158bab47d8e75
geromarq <142598233+geromarq@users.noreply.github.com>
2025-06-12 19:16:09 -0300

    Lista posImp

--- ListaPosIntImp.cpp
+++ ListaPosIntImp.cpp
@@ -4,19 +4,36 @@
 
 //Si necestita otra estructura se puede definir aqui
 
+struct nodoListaPos {
+	int dato;
+	int pos;
+	nodoListaPos* sig;
+	nodoListaPos* ant;
+	nodoListaPos(int dato, unsigned int pos) : dato(dato), pos(pos), sig(NULL), ant(NULL) {};
+	nodoListaPos(int dato, unsigned int pos, nodoListaPos* ant, nodoListaPos* sig) : dato(dato), pos(pos), sig(sig), ant(ant) {};
+};
+
 struct _representacionListaPosInt {
-	//IMPLEMENTAR SOLUCION
+	nodoListaPos* ppio;
+	nodoListaPos* fin;
+	int cant;
+	_representacionListaPosInt() : ppio(NULL), fin(NULL), cant(0) {};
 };
 
 ListaPosInt crearListaPosInt()
 {
-	//IMPLEMENTAR SOLUCION
-	return NULL;
+	return new _representacionListaPosInt();
 }
 
 void agregar(ListaPosInt& l, int e, unsigned int pos)
-{
-	//IMPLEMENTAR SOLUCION
+{	
+	nodoListaPos* nodo = new nodoListaPos(e, pos);
+	nodoListaPos* aux = l->ppio;
+
+	while (aux != NULL) {
+
+	}
+	
 }
 
 void borrar(ListaPosInt& l, unsigned int pos)
