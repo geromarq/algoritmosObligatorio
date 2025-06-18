221bf05e403bf2333d66c56c64c5963f0930f79b
geromarq <geromarqui@gmail.com>
2025-06-18 10:56:49 -0300

    cola prioridad

--- ColaPrioridadIntImp.cpp
+++ ColaPrioridadIntImp.cpp
@@ -8,7 +8,7 @@ struct nodoColaPI {
 	int dato;
 	int prioridad;
 	nodoColaPI* sig;
-	nodoColaPI(int d, int p) : dato(d), prioridad(p), sig(nullptr) {}
+	nodoColaPI(int d, int p) : dato(d), prioridad(p), sig(NULL) {}
 };
 
 struct _representacionColaPrioridadInt {
@@ -16,55 +16,84 @@ struct _representacionColaPrioridadInt {
 	unsigned int largo;    // elementos actuales
 	unsigned int cota;     // capacidad máx.
 
-	_representacionColaPrioridadInt(unsigned int k): ppio(nullptr), largo(0), cota(k) {
-	}
+	_representacionColaPrioridadInt(unsigned int k) : ppio(nullptr), largo(0), cota(k) {};
 };
 
 
 ColaPrioridadInt crearColaPrioridadInt(unsigned int cota) {
-	return new _representacionColaPrioridadInt();
+	return new _representacionColaPrioridadInt(cota);
 }
 
 void encolar(ColaPrioridadInt& c, int e, int p) {
-	// NO IMPLEMENTADO
+	if (esLlena(c))return;
+	nodoColaPI* nodo = new nodoColaPI(e, p);
+	
+
+	if (c->ppio == NULL || p > c->ppio->prioridad) {
+		nodo->sig = c->ppio;
+		c->ppio = nodo;
+	} else {
+		nodoColaPI* aux = c->ppio;
+		while (aux->sig != NULL && aux->sig->prioridad >= p) {
+			aux = aux->sig;
+		}
+		nodo->sig = aux->sig;
+		aux->sig = nodo;
+		delete aux;
+	}
+	
+	c->largo++;
+	
 }
 
 int principio(ColaPrioridadInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	return c->ppio->dato;
 }
 
 int principioPrioridad(ColaPrioridadInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	return c->ppio->prioridad;
 }
 
 void desencolar(ColaPrioridadInt& c) {
-	// NO IMPLEMENTADO
+	nodoColaPI* aBorrar = c->ppio;
+	c->ppio = c->ppio->sig;
+	delete aBorrar;
 }
 
 bool esVacia(ColaPrioridadInt c) {
-	// NO IMPLEMENTADO
-	return true;
+	return c->ppio == NULL;
 }
 
 bool esLlena(ColaPrioridadInt c) {
-	// NO IMPLEMENTADO
-	return false;
+	return c->largo == c->cota;
 }
 
 unsigned int cantidadElementos(ColaPrioridadInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	return c->largo;
 }
 
 ColaPrioridadInt clon(ColaPrioridadInt c) {
-	// NO IMPLEMENTADO
-	return NULL;
+	if (c == NULL)return NULL;
+	ColaPrioridadInt copia = crearColaPrioridadInt(c->cota);
+	for (nodoColaPI* it = c->ppio; it; it = it->sig) { 
+		encolar(copia, it->dato, it->prioridad);
+	}
+	return copia;
 }
 
 void destruir(ColaPrioridadInt& c) {
-	// NO IMPLEMENTADO
+	if (c->ppio == NULL)return;
+	if (c->ppio->sig != NULL) {
+		nodoColaPI* sig = c->ppio->sig;
+		delete c->ppio;
+		c->ppio = sig;
+		destruir(c);
+	} else {
+		delete c->ppio;
+		delete c;
+	}
+	
+
 }
 
 #endif
